cde289d937c81618cbf98b349c72ba756f09d6b5
Yuki Nishijima <mail@yukinishijima.net>
2012-03-25 01:34:24 +0900

    modified intend.

--- contact-example/src/net/yuki24/ContactExample/core/ContactBuilder.java
+++ contact-example/src/net/yuki24/ContactExample/core/ContactBuilder.java
@@ -1,7 +1,6 @@
 package net.yuki24.ContactExample.core;
 
 import java.util.ArrayList;
-
 import android.content.ContentProviderOperation;
 import android.content.ContentProviderResult;
 import android.content.ContentResolver;
@@ -17,38 +16,38 @@ public class ContactBuilder {
 	private ArrayList<ContentProviderOperation> ops = new ArrayList<ContentProviderOperation>();
 	private int rawContactInsertIndex = ops.size();
 
-	public ContentProviderResult[] apply(ContentResolver resolver)	throws RemoteException, OperationApplicationException {
+	public ContentProviderResult[] apply(ContentResolver resolver) throws RemoteException, OperationApplicationException {
 		return resolver.applyBatch(ContactsContract.AUTHORITY, ops);
 	}
 
 	public void assignAccount() {
 		ops.add(ContentProviderOperation.newInsert(RawContacts.CONTENT_URI)
-		.withValue(RawContacts.ACCOUNT_TYPE, null)
-		.withValue(RawContacts.ACCOUNT_NAME, null)
-		.build());
+			.withValue(RawContacts.ACCOUNT_TYPE, null)
+			.withValue(RawContacts.ACCOUNT_NAME, null)
+			.build());
 	}
-	
+
 	public void assignAccount(String accountType, String accountName) {
 		ops.add(ContentProviderOperation.newInsert(RawContacts.CONTENT_URI)
-		.withValue(RawContacts.ACCOUNT_TYPE, accountType)
-		.withValue(RawContacts.ACCOUNT_NAME, accountName)
-		.build());
+			.withValue(RawContacts.ACCOUNT_TYPE, accountType)
+			.withValue(RawContacts.ACCOUNT_NAME, accountName)
+			.build());
 	}
-	
+
 	public void assignPhoneNumber(String phoneNumber) {
-		ops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
-		.withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, rawContactInsertIndex)
-		.withValue(Data.MIMETYPE, Phone.CONTENT_ITEM_TYPE)
-		.withValue(Phone.NUMBER, phoneNumber)
-		.build());
+		ops.add(ContentProviderOperation.newInsert(Data.CONTENT_URI)
+			.withValueBackReference(Data.RAW_CONTACT_ID, rawContactInsertIndex)
+			.withValue(Data.MIMETYPE, Phone.CONTENT_ITEM_TYPE)
+			.withValue(Phone.NUMBER, phoneNumber)
+			.build());
 	}
 	
 	public void assignDisplayName(String displayName) {
 		ops.add(ContentProviderOperation.newInsert(Data.CONTENT_URI)
-		.withValueBackReference(Data.RAW_CONTACT_ID, rawContactInsertIndex)
-		.withValue(Data.MIMETYPE,StructuredName.CONTENT_ITEM_TYPE)
-		.withValue(StructuredName.DISPLAY_NAME, displayName)
-		.build());
+			.withValueBackReference(Data.RAW_CONTACT_ID, rawContactInsertIndex)
+			.withValue(Data.MIMETYPE, StructuredName.CONTENT_ITEM_TYPE)
+			.withValue(StructuredName.DISPLAY_NAME, displayName)
+			.build());
 	}
 	
 	//private static void assignEmail();
